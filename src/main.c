67fbeb81b3ab074db89ce5acf85643df3d9c1e9c
Lance Hart <shipsimfan@hotmail.com>
2022-02-09 11:31:58 -0500

    Fixed color changing bug

--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include <time.h>
 
-const char* months[] = {
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
+const char *months[] = {
+    "January", "February", "March",     "April",   "May",      "June",
+    "July",    "August",   "September", "October", "November", "December",
 };
 
 const int month_lengths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -21,7 +22,7 @@ int get_month_length(int month, int year) {
 
 int main() {
     time_t current_time_epoch = time(NULL);
-    struct tm* date = gmtime(&current_time_epoch);
+    struct tm *date = gmtime(&current_time_epoch);
 
     int num_spaces = (20 - (strlen(months[date->tm_mon]) + 5)) / 2;
 
@@ -31,14 +32,17 @@ int main() {
     printf("%s %i\n", months[date->tm_mon], date->tm_year + 1900);
     printf("Su Mo Tu We Th Fr Sa\n");
 
-    int starting_weekday = day_of_week(date->tm_mon + 1, 1, date->tm_year + 1900);
+    int starting_weekday =
+        day_of_week(date->tm_mon + 1, 1, date->tm_year + 1900);
     for (int i = 0; i < starting_weekday; i++)
         printf("   ");
 
     int current_weekday = starting_weekday;
     int month_length = get_month_length(date->tm_mon, date->tm_year + 1900);
+
     for (int day = 1; day <= month_length; day++) {
         if (day == date->tm_mday) {
+            fflush(stdout);
             console_set_foreground_rgb(0x00, 0x00, 0x00);
             console_set_background_rgb(0xFF, 0xFF, 0xFF);
         }
@@ -46,6 +50,7 @@ int main() {
         printf("%2i", day);
 
         if (day == date->tm_mday) {
+            fflush(stdout);
             console_set_foreground_rgb(0xFF, 0xFF, 0xFF);
             console_set_background_rgb(0x00, 0x00, 0x00);
         }
